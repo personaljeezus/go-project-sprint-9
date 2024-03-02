e02c19ed51c58e07ef7b20d53ef44d63e3ed388f
Denis Kalugin <halfkotla@gmail.com>
2024-03-02 09:50:02 +0300

    Nineteen Ninety Nine

--- precode.go
+++ precode.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -12,30 +13,54 @@ import (
 // вызывается функция fn. Она служит для подсчёта количества и суммы
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
-	// 1. Функция Generator
-	// ...
+	defer close(ch)
+	N := int64(1)
+	for {
+		select {
+		case ch <- N:
+			N++
+			fn(N)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func N(i int) {
+	panic("unimplemented")
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
-	// 2. Функция Worker
-	// ...
+	defer close(out)
+	for {
+		v, ok := <-in
+		if !ok {
+			break
+		}
+		out <- v
+		time.Sleep(1 * time.Millisecond)
+	}
+
 }
 
 func main() {
 	chIn := make(chan int64)
 
 	// 3. Создание контекста
-	// ...
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
 	var inputCount int64 // количество сгенерированных чисел
-
+	var mu sync.RWMutex
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
+		mu.RLock()
 		inputSum += i
 		inputCount++
+		mu.RUnlock()
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -54,8 +79,16 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	// 4. Собираем числа из каналов outs
-	// ...
+	for i, ch := range outs {
+		wg.Add(1)
+		go func(in <-chan int64, i int64) {
+			defer wg.Done()
+			for v := range in {
+				chOut <- v
+				amounts[i]++
+			}
+		}(ch, int64(i))
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -66,10 +99,10 @@ func main() {
 
 	var count int64 // количество чисел результирующего канала
 	var sum int64   // сумма чисел результирующего канала
-
-	// 5. Читаем числа из результирующего канала
-	// ...
-
+	for n := range chOut {
+		count++
+		sum += n
+	}
 	fmt.Println("Количество чисел", inputCount, count)
 	fmt.Println("Сумма чисел", inputSum, sum)
 	fmt.Println("Разбивка по каналам", amounts)
